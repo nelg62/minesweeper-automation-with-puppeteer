6f0bee735c1d3d22c43c9fb804cd0d981903d5e2
nelg62 <159752496+nelg62@users.noreply.github.com>
2025-06-13 16:36:41 +1200

    clicking the game board working

--- minesweeperbot.js
+++ minesweeperbot.js
@@ -1,8 +1,16 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
+  // create new browser create new page
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
+  //   open page to beginner minesweeper
   await page.goto("https://minesweeperonline.com/#beginner");
+
+  //   wait for game to load
+  await page.waitForSelector("#game");
+
+  //   click the top left square in the game
+  await page.click('[id="1_1"]');
 })();
